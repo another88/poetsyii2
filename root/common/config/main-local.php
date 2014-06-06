e7bbc1b9e47f944dcf11da83eab99e79138368d6
Dima Baranov <anotherforwork@gmail.com>
2014-06-06 08:45:27 +0300

    updates

---
+++ root/common/config/main-local.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=yii',
+            'username' => 'root',
+            'password' => 'wer1988',
+            'charset' => 'utf8',
+        ],
+        'mail' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            'useFileTransport' => true,
+        ],
+    ],
+];
